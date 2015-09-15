1bb77a764548b1b57105d53abe7167bd0c25eb17
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-09-15 21:04:25 +0300

    Add views for registering and signing up WIP

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'restaurants/index.html')
+
+def register(request):
+    print("register")
+    return render(request, 'restaurants/register.html')
+
+def login(request):
+    print("login")
+    return render(request, 'restaurants/login.html')
+
