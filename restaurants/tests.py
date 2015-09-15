995e66e632f61fc735df97c9aa23fa8e30051059
Sergei Liski <sergeiliski@gmail.com>
2015-09-16 00:53:43 +0300

    Added example test

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Example test
+class ExampleTestCase(TestCase):
+    # tests need to start with test<name>
+    def test_true_is_true(self):
+        """Testing that true asserts as true"""
+        self.assertEqual(True, True)
