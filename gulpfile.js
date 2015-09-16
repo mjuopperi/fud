6b91051b8cd33c43ba0064cfe0759620968f3b06
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-09-16 12:10:39 +0300

    Add js minification to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
     gulp.src('./restaurants/static/src/sass/**/*.scss')
@@ -9,8 +10,15 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./restaurants/static/restaurants/css'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./restaurants/src/static/sass/**/*.scss', ['sass']);
+gulp.task('compress', function() {
+    return gulp.src('./restaurants/static/src/js/**/*.js')
+        .pipe(uglify({compress : {hoist_vars : true}}))
+        .pipe(gulp.dest('./restaurants/static/restaurants/js'));
 });
 
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('src:watch', function () {
+    gulp.watch('./restaurants/src/static/sass/**/*.scss', ['sass']);
+    gulp.watch('./restaurants/static/src/js/**/*.js', ['compress']);
+});
+
+gulp.task('default', ['sass', 'compress', 'src:watch']);
