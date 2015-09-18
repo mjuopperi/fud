4b14cf2b2a300e3da42d92e673f43a33b9fa2393
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-09-18 19:15:04 +0300

    Add React processing to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp   = require('gulp');
+var watch  = require('gulp-watch');
+var sass   = require('gulp-sass');
+var react  = require('gulp-react');
 var uglify = require('gulp-uglify');
 
+var path = {
+    js_src: './restaurants/static/src/js/**/*.js',
+    js_dest: './restaurants/static/restaurants/js',
+    sass_src: './restaurants/static/src/sass/**/*.scss',
+    sass_dest: './restaurants/static/restaurants/css'
+};
+
+
 gulp.task('sass', function () {
-    gulp.src('./restaurants/static/src/sass/**/*.scss')
+    gulp.src(path.sass_src)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./restaurants/static/restaurants/css'));
+        .pipe(gulp.dest(path.sass_dest));
 });
 
-gulp.task('compress', function() {
-    return gulp.src('./restaurants/static/src/js/**/*.js')
-        .pipe(uglify({compress : {hoist_vars : true}}))
-        .pipe(gulp.dest('./restaurants/static/restaurants/js'));
+gulp.task('js', function () {
+    return gulp.src(path.js_src)
+        .pipe(react())
+        .pipe(uglify())
+        .pipe(gulp.dest(path.js_dest));
 });
 
-gulp.task('src:watch', function () {
-    gulp.watch('./restaurants/src/static/sass/**/*.scss', ['sass']);
-    gulp.watch('./restaurants/static/src/js/**/*.js', ['compress']);
+gulp.task('watch', function () {
+    gulp.watch(path.sass_src, ['sass']);
+    gulp.watch(path.js_src, ['js']);
 });
 
-gulp.task('default', ['sass', 'compress', 'src:watch']);
+gulp.task('default', ['sass', 'js', 'watch']);
