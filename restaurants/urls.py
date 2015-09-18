dd3d30e1decc521f49231e693ab1ab9f0c4d0761
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-09-18 19:18:29 +0300

    Register -> Sign up

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register/?', views.register, name='register'),
+    url(r'^signup/?', views.signup, name='signup'),
     url(r'^login/?', views.login, name='login'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'restaurants/index.html')
 
-def register(request):
-    print("register")
-    return render(request, 'restaurants/register.html')
+def signup(request):
+    return render(request, 'restaurants/signup.html')
 
 def login(request):
-    print("login")
     return render(request, 'restaurants/login.html')
 
