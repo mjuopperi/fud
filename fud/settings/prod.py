05f4a118a96bf72f37dc263b982ad599a9b86e30
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-12-15 22:30:08 +0200

    Store production secrets in secrets.py. /!\ DO NOT PUSH TO GIT /!\

--- fud/settings/prod.py
+++ fud/settings/prod.py
@@ -1,9 +1,28 @@
 from .base import *
+try:
+    from .secrets import *
+except ImportError:
+    pass
 
 DEBUG = False
+
+SECRET_KEY = FUD_SECRET_KEY
 
 BASE_DOMAIN = 'fud.fi'
 
 ALLOWED_HOSTS = {
-    '.fud.fi',
+    '*',
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fud',
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': '5432',
+    }
+}
+
+STATIC_ROOT = '/home/fud/server/static/'
