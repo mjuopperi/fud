dca526d6a8f6e8e9989f832d4e954e3cc127d890
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-12-29 00:04:29 +0200

    Set front page margins with js since vh units are buggy

--- restaurants/static/src/js/index.js
+++ restaurants/static/src/js/index.js
@@ -1,9 +1,26 @@
 require('./_header');
 
+function setMobileMargins() {
+  var header = $('header');
+  var heading = $('#header').find('h1:first-of-type');
+  var arrow = $('.nav-arrow');
+
+  var halfHeight = ($(window).height() - heading.height() - header.height()) / 2;
+  var headingMarginTop = halfHeight * 0.8;
+  var headingMarginBottom = halfHeight - 1.5 * arrow.height();
+  var arrowMarginBottom = $(window).height() - heading.height() - headingMarginTop - headingMarginBottom - arrow.height();
+
+  heading.css('margin-top', headingMarginTop);
+  heading.css('margin-bottom', headingMarginBottom);
+  arrow.css('margin-bottom', arrowMarginBottom);
+}
+
+
 $(function() {
   $('.nav-arrow').click(function() {
     $('html, body').animate({
       scrollTop: $('#features').offset().top
     }, 1000)
   });
+  if ($(window).width() < 600) setMobileMargins();
 });
