2e92563da2343d77cb35d3bcb131565a0352621b
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-01-22 01:01:47 +0200

    Remove example test

--- restaurants/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Example test
-class ExampleTestCase(TestCase):
-    # tests need to start with test<name>
-    def test_true_is_true(self):
-        """Testing that true asserts as true"""
-        self.assertEqual(True, True)
